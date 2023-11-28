403e903b5b161c503d8ece9b66a4477350880cda
jesusabreug <127265825+jesusabreug@users.noreply.github.com>
2023-11-28 13:58:04 -0400

    feat(App.js): add files for App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import { AuthContextWrapper } from './src/Wrappers/AuthContext';
+import Navigation from './src/Wrappers/Navigation';
+
+
+export default function App() {
+  return (
+    <AuthContextWrapper>
+      <Navigation />
+    </AuthContextWrapper>
+  );
+}
+
